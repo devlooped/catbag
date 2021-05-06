a33ddf1e5c3a3f75ba56f54aa78d46c295df6bc0
Daniel Cazzulino <daniel@cazzulino.com>
2021-05-06 18:56:30 -0300

    :cat: AsyncLazy: added basically from Stephen's blog
    Basically taken from https://devblogs.microsoft.com/pfxteam/asynclazyt/.
    
    Usage:
    
        var lazy = AsyncLazy.Create(() => ...);
        var value = await lazy.Value;
    
    Or
    
        var lazy = new AsyncLazy<Foo>(() => ...);
    
    The factory methods are convenient, just like KeyValuePair.Create :)

---
+++ tests/System/Threading/Tasks/AsyncLazyTests.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+
+namespace System.Threading.Tasks
+{
+    public class AsyncLazyTests
+    {
+        [Fact]
+        public async Task AwaitValue()
+        {
+            var lazy = AsyncLazy.Create(() => Task.FromResult(25));
+
+            Assert.Equal(25, await lazy.Value);
+        }
+
+        [Fact]
+        public async Task AwaitAsyncValue()
+        {
+            var lazy = AsyncLazy.Create(async () => await Task.FromResult(25));
+
+            Assert.Equal(25, await lazy.Value);
+        }
+
+        [Fact]
+        public async Task AwaitNewLazyValue()
+        {
+            var lazy = new AsyncLazy<int>(() => Task.FromResult(25));
+
+            Assert.Equal(25, await lazy.Value);
+        }
+
+        [Fact]
+        public async Task AwaitNewLazyAsyncValue()
+        {
+            var lazy = new AsyncLazy<int>(async () => await Task.FromResult(25));
+
+            Assert.Equal(25, await lazy.Value);
+        }
+    }
+}
