6804fe18d13b1473cb1bdcb6d58493ae25128a3d
Daniel Cazzulino <daniel@cazzulino.com>
2021-05-17 19:11:58 -0300

    :cat: String.FormatNamed: allow replacing named tokens in a string template
    Both anonymous object and dictionary are supported for formatting.
    For object-based formatting, properties are matched in a case-insensitive manner.

---
+++ tests/System/String.FormatNamedTests.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+#nullable enable
+namespace System
+{
+    public class StringFormatNamed
+    {
+        [Fact]
+        public void ReplaceWithDictionary()
+        {
+            var template = "https://foo.com/{path}/{value:#,#.##}/{id:n}";
+            var id = Guid.NewGuid();
+            var path = "folder";
+            var value = 42.424242;
+            var values = new Dictionary<string, object?>
+            {
+                { "path", path},
+                { "value", value },
+                { "id", id }
+            };
+
+            var actual = template.FormatNamed(new()
+            {
+                { "path", path },
+                { "value", value },
+                { "id", id }
+            });
+
+            Assert.Equal($"https://foo.com/{path}/{value:#,#.##}/{id:n}", actual);
+        }
+
+        [Fact]
+        public void ReplaceWithDictionarySupportsNulls()
+        {
+            var template = "https://foo.com/{path}/{value:#,#.##}/{id:n}";
+            Guid? id = null;
+            string? path = null;
+            double? value = null;
+            var values = new Dictionary<string, object?>
+            {
+                { "path", path },
+                { "value", value },
+                { "id", id }
+            };
+
+            var actual = template.FormatNamed(values);
+            Assert.Equal($"https://foo.com/{path}/{value:#,#.##}/{id:n}", actual);
+        }
+
+        [Fact]
+        public void ReplaceWithAnonymousObject()
+        {
+            var template = "https://foo.com/{path}/{value:#,#.##}/{id:n}";
+            var id = Guid.NewGuid();
+            var path = "folder";
+            var value = 42.424242;
+            var values = new
+            {
+                path = path,
+                value = value,
+                id = id
+            };
+
+            var actual = template.FormatNamed(values);
+            Assert.Equal($"https://foo.com/{path}/{value:#,#.##}/{id:n}", actual);
+        }
+
+        [Fact]
+        public void ReplaceWithAnonymousObjectCaseInsensitive()
+        {
+            var template = "https://foo.com/{path}/{value:#,#.##}/{id:n}";
+            var id = Guid.NewGuid();
+            var path = "folder";
+            var value = 42.424242;
+            var values = new
+            {
+                Path = path,
+                Value = value,
+                ID = id
+            };
+
+            var actual = template.FormatNamed(values);
+            Assert.Equal($"https://foo.com/{path}/{value:#,#.##}/{id:n}", actual);
+        }
+    }
+}
