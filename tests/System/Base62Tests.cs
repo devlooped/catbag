43712304cdfff2c34ba01197cc2ccc0e16e7af08
Daniel Cazzulino <daniel@cazzulino.com>
2021-09-24 17:00:50 -0300

    :cat: Add Base62 encoding/decoding feature
    Usage is straightforward: Base62.Encode/Decode. A BitInteger is used so (mostly) arbitrarily large values can be encoded into concise strings.

---
+++ tests/System/Base62Tests.cs
@@ -0,0 +1,24 @@
+﻿#nullable enable
+using System.Collections.Generic;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace System
+{
+    public record Base62Tests(ITestOutputHelper Output)
+    {
+        [Fact]
+        public void CanRoundtripValue()
+        {
+            var expected = DateTimeOffset.UtcNow.Ticks;
+
+            var code = Base62.Encode(expected);
+
+            Output.WriteLine(code);
+
+            var actual = Base62.Decode(code);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
