0fb45ad926b9e12897cedda3c5093ea958cc3638
Daniel Cazzulino <daniel@cazzulino.com>
2021-09-25 20:58:09 -0300

    :cat: PreciseTime: a high-precision UtcNow
    Provides a tenths of microseconds-precision timing for
    UtcNow.
    
    Usage:
    
    ```csharp
    var now1 = PreciseTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss.fffffffZ");
    var now2 = PreciseTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss.fffffffZ");
    ```
    
    The two values will never be the same, since the current
    stopwatch frequence is taken into account for calculating
    (within the same process) the precise time.

---
+++ tests/System/PreciseTimeTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace System
+{
+    public class PreciseTimeTests
+    {
+        ITestOutputHelper output;
+
+        public PreciseTimeTests(ITestOutputHelper output) => this.output = output;
+
+        [Fact]
+        public void WhenGettingNow_ThenThereAreNeverDuplicates()
+        {
+            for (var i = 0; i < Stopwatch.Frequency; i++)
+            {
+                var first = PreciseTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss.fffffffZ");
+                var second = PreciseTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss.fffffffZ");
+
+                Assert.NotEqual(first, second);
+            }
+
+            output.WriteLine($"Run for {Stopwatch.Frequency} iterations");
+        }
+    }
+}
