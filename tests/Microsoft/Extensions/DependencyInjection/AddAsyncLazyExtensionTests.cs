2f8a7d3dffc4409dbda61afb43326ab9d871c1ec
Daniel Cazzulino <daniel@cazzulino.com>
2022-06-29 16:44:53 -0300

    🐱 Add IServiceCollection.AddAsyncLazy() extension method
    This allows initializing any service asynchronously and consuming it using `AsyncLazy<T>` as a dependency instead of the direct service type.
    
    ```csharp
    services.AddAsyncLazy();
    services.AddSingleton(async sp =>
    {
       // async init with await(s) as needed
      return service;
    });
    ```
    
    A component could declare a dependency like:
    
    ```csharp
    public MyController(AsyncLazy<IMyExpensiveService> service)
    ```
    
    And upon needing the async-initialized service, the controller (in this case) would simply await the lazy:
    
    ```csharp
    var instance = await service;
    instance.Do(...);
    ```

---
+++ tests/Microsoft/Extensions/DependencyInjection/AddAsyncLazyExtensionTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public class AddAsyncLazyExtensionTests
+    {
+        [Fact]
+        public async void CanResolveLazily()
+        {
+            var services = new ServiceCollection();
+            services.AddAsyncLazy()
+                .AddSingleton(async _ =>
+                {
+                    await Task.Delay(1);
+                    return Mock.Of<IMyService>();
+                })
+                .AddTransient<Lazily>();
+
+            var container = services.BuildServiceProvider();
+
+            var lazy = container.GetRequiredService<Lazily>();
+
+            Assert.Same(await lazy.MyService, await lazy.MyService.Value);
+        }
+
+        public interface IMyService { }
+
+        record Lazily(AsyncLazy<IMyService> MyService);
+    }
+}
