ae3164bd2fcedb153de8cc33c7d9d62c9043a41e
Daniel Cazzulino <daniel@cazzulino.com>
2022-06-29 16:14:57 -0300

    🐱 Add IServiceCollection.AddLazy() extension method
    This allows resolving any service lazily using `Lazy<T>` as a dependency instead of the direct service type.
    
    ```csharp
    services.AddLazy();
    services.AddSingleton<IMyExpensiveService>(() => // expensive creation here);
    ```
    
    A component could declare a dependency like:
    
    ```csharp
    public MyController(Lazy<IMyExpensiveService> service)
    ```
    
    And the registration side of the service wouldn't change at all.
    
    The benefit of depending on the Lazy<T> is that the controller (in this example) might not end up needing the expensive service at all, for example. And using the lazy in this case, would avoid entirely the service creation.

---
+++ tests/Microsoft/Extensions/DependencyInjection/AddLazyExtensionTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Moq;
+using Xunit;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public class AddLazyExtensionTests
+    {
+        [Fact]
+        public void CanResolveLazily()
+        {
+            var services = new ServiceCollection();
+            services.AddLazy()
+                .AddSingleton(_ => Mock.Of<IMyService>())
+                .AddTransient<Lazily>();
+
+            var container = services.BuildServiceProvider();
+
+            var lazy = container.GetRequiredService<Lazily>();
+
+            Assert.NotNull(lazy.MyService.Value);
+        }
+
+        public interface IMyService { }
+
+        record Lazily(Lazy<IMyService> MyService);
+    }
+}
