6ea1a121c1e5de55af14800eb0c7e0664c4e9a18
Daniel Cazzulino <daniel@cazzulino.com>
2023-06-09 16:41:14 -0300

    🐱 Add Lorem Ipsum generator
    Allows generating random text.

---
+++ tests/System/IpsumTests.cs
@@ -0,0 +1,32 @@
+﻿#nullable enable
+using Xunit;
+
+namespace System
+{
+    public class IpsumTests
+    {
+        [Fact]
+        public void WhenGettingWords_ThenEndsWithDot()
+        {
+            var phrase = Ipsum.GetPhrase(10);
+
+            Assert.EndsWith(".", phrase);
+        }
+
+        [Fact]
+        public void WhenGetting10Words_ThenStartsWithLoremIpsum()
+        {
+            var phrase = Ipsum.GetPhrase(10);
+
+            Assert.StartsWith("Lorem ipsum dolor sit amet", phrase);
+        }
+
+        [Fact]
+        public void WhenGetting3Words_ThenStartsWithLoremIpsumDolor()
+        {
+            var phrase = Ipsum.GetPhrase(3);
+
+            Assert.Equal("Lorem ipsum dolor.", phrase);
+        }
+    }
+}
